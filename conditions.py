563948b710a18117c8ff17788b30bd6d8c741ff2
skhomuti <skhomuti@gmail.com>
2023-04-18 21:31:43 +0500

    Init project

---
+++ conditions.py
@@ -0,0 +1,14 @@
+
+# Boolean - 3 состояния
+
+b = bool
+
+t = True
+f = False
+n = None
+
+# if/elif/else - на примере кода ответа
+
+code = 500
+
+# Пустые объекты - false
---
+++ loops.py
@@ -0,0 +1,29 @@
+
+# While - цикл с предусловием
+# пока пользователь не введет правильный пароль, ...
+import random
+
+actual_password = 12345
+password_from_user = 54321
+
+password_incorrect = actual_password == password_from_user
+
+
+# For. Итерируем списки и словари
+
+users = [1, 2, 3, 4, 5]
+
+d = {
+    "key": 123,
+    "another": 456,
+    "third": 789
+}
+
+
+# Break/Continue/Else - прерывание цикла
+
+# for i in range - цикл с итератором
+
+# enumerate - возвращает пары (индекс, значение)
+
+cities = ["Екатеринбург", "Москва", "Сочи"]
